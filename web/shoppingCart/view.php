41c29a97f9fc0a854a16614203ffa4dd7470c651
Christian Rahman <christian.rahman@gmail.com>
2017-05-06 16:05:46 -0700

    first push for shopping cart

--- web/shoppingCart/view.php
+++ web/shoppingCart/view.php
@@ -2,9 +2,18 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Teach 03</title>
+    <title>Info</title>
   </head>
   <body>
-    
+    <?php
+      $name = $_POST["name"];
+      $email = $_POST["email"];
+      $major = $_POST["major"];
+      $comments = $_POST["comments"];
+     <p><?php echo $name; ?></p>
+     <p><?php echo $email; ?></p>
+     <p><?php echo $major; ?></p>
+     <p><?php echo $comments; ?></p>
+
   </body>
 </html>
